d51c0dcad98d4a599da6a7e44051b39ede004d36
Thibault Jouannic <thibault@miximum.fr>
2013-04-17 12:59:42 +0200

    specific page for each point of interest

--- city_map/urls.py
+++ city_map/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns("city_map.views",
     url('^$', 'map', name='map'),
+    url('^(?P<id>\d+)/$', 'point_of_interest', name='point_of_interest'),
 )
 
--- city_map/views.py
+++ city_map/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from mezzanine.utils.views import render
 from city_map.models import PointOfInterest
 
@@ -6,4 +7,12 @@ def map(request):
     pois = PointOfInterest.objects.all()
     return render(request, 'map.html', {
             'pois': pois,
+    })
+
+def point_of_interest(request, id):
+    pois = PointOfInterest.objects.all()
+    selected_poi = get_object_or_404(PointOfInterest, id=id)
+    return render(request, 'map.html', {
+            'pois': pois,
+            'selected_poi': selected_poi,
     })
