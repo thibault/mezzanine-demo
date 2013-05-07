349062c03e46c44a231512be9bc6a59352090f38
Thibault Jouannic <thibault@miximum.fr>
2013-05-07 15:27:01 +0200

    clean backbone app structure

--- city_map/static/js/map/app.js
+++ city_map/static/js/map/app.js
@@ -3,21 +3,15 @@ var App = {
     Collections: {},
     Views: {},
     Routers: {},
-    Config: {}
-};
-
-App.Routers.MapRouter = Backbone.Router.extend({
-    initialize: function(options) {
-        this.route('', 'map');
-        this.route(':slug/', 'pointOfInterest');
-    },
-    map: function() {
-        console.log('map');
-    },
-    pointOfInterest: function(slug) {
-        console.log('point !');
+    Config: {},
+    start: function(map) {
+        var mapRouter = new App.Routers.MapRouter({ map: map });
+        Backbone.history.start({
+            pushState: true,
+            root: App.Config.mapUrl,
+        });
     }
-});
+};
 
 App.Models.Marker = Backbone.Model.extend({
     // name
@@ -31,13 +25,19 @@ App.Collections.MarkerCollection = Backbone.Collection.extend({
     initialize: function() {
         this.url = App.Config.markerCollectionUrl;
     },
+    getBySlug: function(slug) {
+        return this.findWhere({ slug: slug });
+    }
 });
 
 //MarkerView has no element
 App.Views.MarkerView = Backbone.View.extend({
 
+    id: "map-data",
     initialize: function(options) {
         this.map = options.map,
+
+        // Create the marker using leaflet api
         this.marker = L.marker([this.model.get('lat'), this.model.get('lng')]);
 
         // Create the marker popup
@@ -46,20 +46,17 @@ App.Views.MarkerView = Backbone.View.extend({
         popup.innerHTML = this.model.get('name');
 
         // We have to bind the event here, because leaflets prevents
-        // the click event to buble outside the map element
+        // the click event to bubble outside the map element
         var slug = this.model.get('slug');
         popup.onclick = function(event) {
             event.preventDefault();
-            Backbone.history.navigate(slug);
+            Backbone.history.navigate(slug + '/', true);
         };
         this.marker.bindPopup(popup);
-    },
 
-    render: function() {    
-        // All we have to do is add the marker to the map
+        // Render the marker to the map
         // (Leaflet API)
         this.marker.addTo(this.map);
-        return this;
     }
 });
 
@@ -70,6 +67,7 @@ App.Views.MapView = Backbone.View.extend({
         this.markers = new App.Collections.MarkerCollection();
         this.listenTo(this.markers, "reset", this.render);
         this.markers.fetch({ reset: true });
+        this.markerViews = [];
     },
     render: function() {
         this.markers.each(this.addOne, this);
@@ -80,14 +78,26 @@ App.Views.MapView = Backbone.View.extend({
             model: marker,
             map: this.map
         });
-        view.render();
+        this.markerViews.push(view);
     }
 });
 
-$(function() {
-    var mapRouter = new App.Routers.MapRouter();
-    Backbone.history.start({
-        pushState: true,
-        root: App.Config.mapUrl,
-    });
+App.Routers.MapRouter = Backbone.Router.extend({
+    initialize: function(options) {
+        this.route('', 'map');
+        this.route(':slug/', 'pointOfInterest');
+        this.mapView = new App.Views.MapView({ map: options.map });
+    },
+    map: function() {
+        // Display the basic map information
+        console.log('Router: map');
+    },
+    pointOfInterest: function(slug) {
+        // Display a single point of interest
+        console.log('Router: poi ' + slug);
+
+        var marker = this.mapView.markers.getBySlug(slug);
+        // Find model from collection by slug
+        // Render model
+    }
 });
